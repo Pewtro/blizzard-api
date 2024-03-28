35e755a0b4a4cba2e21dffac5431644106c5ab56
Putro <29204244+Pewtro@users.noreply.github.com>
2024-03-28 12:04:36 +0100

    [Client] Update tests to new types

--- packages/client/src/tests/wow/achievement.test.ts
+++ packages/client/src/tests/wow/achievement.test.ts
@@ -13,7 +13,7 @@ describe('achievement', async () => {
   client.setAccessToken(access.data.access_token);
 
   it('should be able to fetch an achievement', async () => {
-    const response = await client.sendRequest(wow.achievements.achievementCategory(16_542)).catch((error) => {
+    const response = await client.sendRequest(wow.achievementCategory(16_542)).catch((error) => {
       console.error('error', error);
       throw new Error('Failed to fetch achievement');
     });
