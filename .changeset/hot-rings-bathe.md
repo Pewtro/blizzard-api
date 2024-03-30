8340154d8496c9defeb691784385994aab505b75
Putro <29204244+Pewtro@users.noreply.github.com>
2024-03-30 21:43:09 +0100

    [Client] Add a few tests to the client

---
+++ .changeset/hot-rings-bathe.md
@@ -0,0 +1,5 @@
+---
+'@blizzard-api/client': patch
+---
+
+Add a few more tests to the client for newly added wow endpoints
---
+++ packages/client/src/tests/wow.test.ts
@@ -0,0 +1,30 @@
+import { wow } from '@blizzard-api/wow';
+import { describe, expect, it } from 'vitest';
+import { environment } from '../../../../environment';
+import { BlizzardApiClient } from '../client';
+
+describe('wow paths and types', async () => {
+  const client = new BlizzardApiClient({
+    key: environment.blizzardClientId,
+    secret: environment.blizzardClientSecret,
+    origin: 'eu',
+  });
+  const access = await client.getAccessToken();
+  client.setAccessToken(access.data.access_token);
+
+  it('should be able to fetch an achievement', async () => {
+    const response = await client.sendRequest(wow.achievement(16_542));
+
+    expect(response.data).toBeDefined();
+  });
+  it('should be able to fetch from the auction house', async () => {
+    const response = await client.sendRequest(wow.auctions(3692));
+
+    expect(response.data).toBeDefined();
+  });
+  it('should be able to fetch a connected realm', async () => {
+    const response = await client.sendRequest(wow.connectedRealm(3692));
+
+    expect(response.data).toBeDefined();
+  });
+});
