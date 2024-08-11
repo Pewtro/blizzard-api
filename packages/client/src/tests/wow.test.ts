ad5f50b1433d92ec9abb7234da9c1b0aea52a714
Putro <29204244+Pewtro@users.noreply.github.com>
2024-08-11 03:16:16 +0200

    [WoW] Character Soulbind APIs are not protected and add JSDoc

--- packages/client/src/tests/wow.test.ts
+++ packages/client/src/tests/wow.test.ts
@@ -205,4 +205,9 @@ describe.concurrent('smoketest some wow api responses', async () => {
     const response = await client.sendRequest(wow.characterReputationsSummary(defaultRealm, defaultCharacter));
     expect(response.data).toBeDefined();
   });
+
+  it("can search for a character's soulbinds", async ({ expect }) => {
+    const response = await client.sendRequest(wow.characterSoulbinds(defaultRealm, defaultCharacter));
+    expect(response.data).toBeDefined();
+  });
 });
--- packages/wow/src/character-soulbinds/character-soulbinds.ts
+++ packages/wow/src/character-soulbinds/character-soulbinds.ts
@@ -1,14 +1,14 @@
-import type { ProtectedResource } from '@blizzard-api/core';
+import type { Resource } from '@blizzard-api/core';
 import type { CharacterSoulbindsResponse } from './types';
 
-export function characterSoulbinds(
-  realmSlug: string,
-  characterName: string,
-  token: string,
-): ProtectedResource<CharacterSoulbindsResponse> {
+/**
+ * @param realmSlug The slug of the realm.
+ * @param characterName The lowercase name of the character.
+ * @returns a character's soulbinds.
+ */
+export function characterSoulbinds(realmSlug: string, characterName: string): Resource<CharacterSoulbindsResponse> {
   return {
     namespace: 'profile',
     path: `/profile/wow/character/${realmSlug}/${characterName.toLowerCase()}/soulbinds`,
-    token,
   };
 }
