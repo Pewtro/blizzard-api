21becc52e115eb933fa32ebc0bac9d1b95c2dd30
Putro <29204244+Pewtro@users.noreply.github.com>
2024-08-11 03:06:42 +0200

    [WoW] Character Mythic Keystone Profile APIs are not protected and add JSDoc

--- packages/wow/src/character-mythic-keystone-profile/character-mythic-keystone-profile.ts
+++ packages/wow/src/character-mythic-keystone-profile/character-mythic-keystone-profile.ts
@@ -1,30 +1,39 @@
-import type { ProtectedResource } from '@blizzard-api/core';
+import type { Resource } from '@blizzard-api/core';
 import type {
   CharacterMythicKeystoneProfileIndexResponse,
   CharacterMythicKeystoneSeasonDetailsResponse,
 } from './types';
 
+/**
+ * @param realmSlug The slug of the realm.
+ * @param characterName The lowercase name of the character.
+ * @returns Returns the Mythic Keystone season details for a character. Returns a 404 Not Found for characters that have not yet completed a Mythic Keystone dungeon for the specified season.
+ */
 export function characterMythicKeystoneProfileIndex(
   realmSlug: string,
   characterName: string,
-  token: string,
-): ProtectedResource<CharacterMythicKeystoneProfileIndexResponse> {
+): Resource<CharacterMythicKeystoneProfileIndexResponse> {
   return {
     namespace: 'profile',
     path: `/profile/wow/character/${realmSlug}/${characterName}/mythic-keystone-profile`,
-    token,
   };
 }
 
+/**
+ * Returns the Mythic Keystone season details for a character.
+ * Returns a 404 Not Found for characters that have not yet completed a Mythic Keystone dungeon for the specified season.
+ * @param realmSlug The slug of the realm.
+ * @param characterName The lowercase name of the character.
+ * @param seasonId The ID of the season.
+ * @returns the Mythic Keystone season details for a character.
+ */
 export function characterMythicKeystoneSeasonDetails(
   realmSlug: string,
   characterName: string,
   seasonId: number,
-  token: string,
-): ProtectedResource<CharacterMythicKeystoneSeasonDetailsResponse> {
+): Resource<CharacterMythicKeystoneSeasonDetailsResponse> {
   return {
     namespace: 'profile',
     path: `/profile/wow/character/${realmSlug}/${characterName}/mythic-keystone-profile/season/${seasonId}`,
-    token,
   };
 }
