814f445485933b22faf85925676c941be1876855
Putro <29204244+Pewtro@users.noreply.github.com>
2024-12-18 16:38:43 +0100

    [Tests] Skip some tests while WoW API is scuffy

--- packages/client/src/tests/classic-wow.test.ts
+++ packages/client/src/tests/classic-wow.test.ts
@@ -10,15 +10,23 @@ describe.concurrent('smoketest some classic wow api responses', async () => {
     secret: environment.blizzardClientSecret,
   });
 
-  it('should be able to fetch the auction house index', async ({ expect }) => {
+  //TODO This is not working, it returns a 404 - it does the same in the official documentation
+  it.skip('should be able to fetch the auction house index', async ({ expect }) => {
     const response = await client.sendRequest(classicWow.auctionHouseIndex('dynamic-classic', 4440));
 
     expect(response.data).toBeDefined();
   });
 
+  //TODO This is not working, it returns a 404 - it does the same in the official documentation
   //This response is really large so it might sometimes time out, so we increase the timeout from the default 5 seconds to 20 seconds
-  it('should be able to fetch the auction house data', { timeout: 20_000 }, async ({ expect }) => {
+  it.skip('should be able to fetch the auction house data', { timeout: 20_000 }, async ({ expect }) => {
     const response = await client.sendRequest(classicWow.auctions('dynamic-classic', 4440, 2));
+
+    expect(response.data).toBeDefined();
+  });
+
+  it('should be able to fetch the classic wow realms', async ({ expect }) => {
+    const response = await client.sendRequest(classicWow.realmIndex('dynamic-classic1x'));
 
     expect(response.data).toBeDefined();
   });
