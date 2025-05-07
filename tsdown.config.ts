cadb78536a64db2c0b607ac083e7f63c217c6425
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-05-07 16:58:25 +0200

    [Project] Migrate to tsdown for building

---
+++ tsdown.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig(({ watch = false }) => ({
+  clean: true,
+  dts: true,
+  entry: {
+    index: './src/index.ts',
+  },
+  external: [],
+  format: 'esm',
+  outDir: 'dist',
+  sourcemap: true,
+  treeshake: true,
+  watch,
+}));
