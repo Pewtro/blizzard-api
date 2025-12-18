be761851be287fa46d7081c1d1f00cd47033bcbf
Pewtro <29204244+Pewtro@users.noreply.github.com>
2025-12-18 12:28:56 +0100

    [Project] Update dev dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import { configs as putstackConfigs } from '@putstack/eslint-config-typescript';
 import jsdocEslint from 'eslint-plugin-jsdoc';
-import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['**/coverage/**', '**/dist/**', '**/node_modules/**', '**/build/**', '**/docs/**', '!.prettierrc.js'],
   },
