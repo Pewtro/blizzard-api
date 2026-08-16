4797458100d93659bca88582230a02e72124eb29
Pewtro <29204244+Pewtro@users.noreply.github.com>
2026-08-16 17:07:05 +0200

    [Project] Fix vitest config not being passed to projects

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,16 +10,32 @@ export default defineConfig({
         ...defaultExclude,
         //There's no reason to test the index files as they are just exporting what is already tested
         '**/index.ts',
-        //Ignore the config files
-        '**/.prettierrc.js',
-        '**/vitest.workspace.ts',
+        //The generated folder is to help with tests not something to be tested itself
+        'generated/**',
+        //Ignore the root files for coverage purposes
+        '*.js',
+        '*.ts',
       ],
       provider: 'v8',
       reporter: ['html', 'lcov'],
     },
-    globals: true,
+    isolate: false,
     // Limit the number of concurrent test runs to avoid overwhelming the blizzard API with requests that could cause 429.
     maxWorkers: '50%',
-    projects: ['packages/*'],
+    projects: [
+      {
+        test: {
+          include: ['packages/**/*.test.ts', '!packages/integration-tests'],
+          name: 'unit',
+        },
+      },
+      {
+        test: {
+          include: ['packages/integration-tests/**/*.test.ts'],
+          name: 'integration',
+        },
+      },
+    ],
+    testTimeout: 20_000,
   },
 });
